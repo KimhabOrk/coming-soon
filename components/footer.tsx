9fd41ae6679c019aa4765e106e76a1b3bde983a9
kimhabork <kimhab.ork@yahoo.com>
2026-03-04 02:19:20 +0700

    update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
-import { Instagram, Twitter, Mail } from "lucide-react";
+import { Instagram, Facebook, Linkedin } from "lucide-react";
 import Link from "next/link"
+import Image from "next/image"
 
 export function Footer() {
   return (
