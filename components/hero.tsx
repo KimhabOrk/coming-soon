4db987e0e9dcf2eff58d8e2e3cc412cf435fec5c
kimhabork <kimhab.ork@yahoo.com>
2026-03-05 14:09:05 +0700

    update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,7 +22,7 @@ export function Hero() {
         }
       `}</style>
       
-    <main className="relative z-10 min-h-screen h-screen px-4 md:px-20 lg:px-24 pt-12 md:pt-14">
+    <main className="relative z-10 min-h-screen h-screen px-4 md:px-20 lg:px-24 pt-18 md:pt-16">
       {/* Logo/Brand section */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-center my-8 md:my-16">
         <div className="relative flex items-center justify-center mx-auto hidden md:block lg:block">
@@ -63,7 +63,7 @@ export function Hero() {
               </h2>
 
               <p
-                className="text-sm md:text-lg text-gray-300 font-light leading-tight mx-auto uppercase tracking-[0.15em]"
+                className="text-sm md:text-lg text-gray-300 font-light leading-tight mx-auto uppercase"
                 style={{
                   animation: "fadeInUp 1.2s ease-out",
                 }}
