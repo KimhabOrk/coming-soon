a09629b51ee1d1766ebbb8b637a8828645781ee5
kimhabork <kimhab.ork@yahoo.com>
2026-03-08 01:08:28 +0700

    update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: '*',
-			allow: '/',
-		},
+		rules: [
+			{
+				userAgent: '*',
+				allow: '/',
+			},
+			{
+				userAgent: 'Algolia-Crawler-Verif',
+				allow: '05ACC8FD982A720C',
+			},
+		],
 		sitemap: [
 			'https://www.ppfi.art/sitemap.xml',
 			'https://www.ppfi.art/sitemap-0.xml'
