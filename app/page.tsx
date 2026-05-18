ec6f00ec3c97f4c1c075e89ac612de7c5eccb3b1
kimhabork <kimhab.ork@yahoo.com>
2026-05-18 14:40:38 +0700

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,17 @@ export default function Home() {
         <div className="absolute inset-0">
           <Image
             src="https://ik.imagekit.io/kimhabork/assets/portrait-bg.png"
-            className="w-full h-full object-fit md:hidden lg:hidden opacity-60"
-            fill
+            className="w-full h-full object-cover md:hidden lg:hidden opacity-60"
+            height={1169}
+            width={826}
             alt="background image"
             priority
           />
           <Image
             src="https://ik.imagekit.io/kimhabork/assets/landscape-bg.png"
-            className="w-full h-full object-fit hidden md:block lg:block opacity-60"
-            fill
+            className="w-full h-full object-cover hidden md:block lg:block opacity-60"
+            height={1080}
+            width={1920}
             alt="background image"
             priority
           />
